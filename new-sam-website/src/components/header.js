378553c26ffb4e57791e156486f6a5f92f6971b9
Samuel Lanctot <lanctotsm@gmail.com>
2021-08-06 18:13:12 -0400

    Add new components

---
+++ new-sam-website/src/components/header.js
@@ -0,0 +1,20 @@
+//<!-- Masthead-->
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (<header class="masthead bg-primary text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+                <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="..." />
+                <h1 class="masthead-heading text-uppercase mb-0">Start Bootstrap</h1>
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <p class="masthead-subheading font-weight-light mb-0">Full Stack Developer in .Net</p>
+            </div>
+        </header>
+        );
+    }
+}
